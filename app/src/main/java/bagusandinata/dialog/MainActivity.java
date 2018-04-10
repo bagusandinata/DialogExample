965985830b98ca4ed6ccbcc53c72bc900668e7f8
bagus anugerah dinata <bagus.anugerah.dinata@gmail.com>
2018-04-10 11:21:27 +0700

    alert dialog & confirmation dialog

---
+++ app/src/main/java/bagusandinata/dialog/MainActivity.java
@@ -0,0 +1,101 @@
+package bagusandinata.dialog;
+
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    AlertDialog alertDialog;
+    AlertDialog.Builder builder;
+    String[] items = {"Easy", "Medium", "Hard", "Very Hard"};
+    String results = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //alert dialog
+//        getAlertDialog();
+
+        //confirmation dialog
+        getConfirmationDialog();
+
+
+    }
+
+    private void getAlertDialog(){
+        //if you change theme button please create R.style.blabla
+        builder = new AlertDialog.Builder(MainActivity.this, R.style.AlertDialogTheme);
+
+        //message dialog
+        builder.setMessage("Discard draft");
+
+        //set positive button
+        builder.setPositiveButton("DISCARD", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(getApplicationContext(), "discard", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        //set negative button
+        builder.setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(getApplicationContext(), "cancel", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        //create dialog
+        alertDialog = builder.create();
+
+        //show dialog
+        alertDialog.show();
+
+        //set color positive button
+        //alertDialog.getButton(AlertDialog.BUTTON_POSITIVE).setTextColor(getResources().getColor(R.color.colorAlertDialog));
+    }
+
+    private void getConfirmationDialog(){
+        //if you change theme button please create R.style.blabla
+        builder = new AlertDialog.Builder(MainActivity.this, R.style.ConfirmationDialogTheme);
+
+        //set title dialog
+        builder.setTitle("Select the difficulty level");
+
+        //set choice dialog
+        builder.setSingleChoiceItems(items, -1, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                results = items[which];
+            }
+        });
+
+        //set positive button
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(getApplicationContext(), results, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        //set negative button
+        builder.setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(getApplicationContext(), "CANCEL", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        //create dialog
+        alertDialog = builder.create();
+
+        //show dialog
+        alertDialog.show();
+    }
+
+}
